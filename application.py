45c719f605e997b17873dc53c4e86c78891739de
Franziska von der Goltz <franziska@vdgoltz.eu>
2016-10-16 00:40:01 +0000

    initial commit. first draft of completed flask excercise

---
+++ application.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+import jinja2
+
+
+app = Flask(__name__)
+
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+@app.route("/")
+def index_page():
+    """Show an index page."""
+
+    return render_template("index.html")
+
+
+@app.route("/application")
+def application_form():
+    """The application form is displayed and let's applicants fill it out"""
+
+    return render_template("/application-form.html")
+
+
+@app.route("/application-response", methods=["POST"])
+def application():
+    """Summarizes the application details provided in application_form"""
+
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    salary = request.form.get('salary')
+    position = request.form.get('position')
+
+    return render_template("/application-response.html", firstname=firstname, lastname=lastname,
+                           salary=salary, position=position)
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
